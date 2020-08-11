370ae702f714518c818b1e84c938ad006735fd82
Songleen <sellee7092@163.com>
2020-08-11 16:03:56 +0800

    修复了一个bug

---
+++ src/main/java/MyDemoClass01.java
@@ -0,0 +1,11 @@
+/**
+ * @Describe
+ * @ClassName
+ * @Author 李松林
+ * @Date 2020/8/11 16:01
+ */
+public class MyDemoClass01 {
+    public static void main(String[] args) {
+        System.out.println("打印一行数据！！");
+    }
+}
