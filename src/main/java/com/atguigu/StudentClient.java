f6b10415da7ba9593c85a80a39bce6e70de4d2de
Songleen <sellee7092@163.com>
2022-03-13 12:10:29 +0800

    添加git的忽略文件

---
+++ src/main/java/com/atguigu/StudentClient.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+/**
+ * @ClassName JavaBasic
+ * @Author Songleen
+ * @Date 2022/03/13/12:06
+ */
+public class StudentClient {
+    public static void main(String[] args) {
+        System.out.println("只是打印一段话");
+    }
+}
